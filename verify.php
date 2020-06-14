e9010552f30fc9f0bac06c5189dd737c552e7cbf
beeyokee <66898856+beeyokee@users.noreply.github.com>
2020-06-14 22:05:30 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php 
-  $access_token = 'w3VOshqIzVxUguZeL7qYVQGGktHGE8qHRHlcX1DS88jCiNe9LYVSdhNd3M4BGZ38KaD0cE7rCa/d0Vb5ic7aVJ/zhhVHJi28qKDp3CPfmcv1JiQBDk9J8YzbsH1ZfTrDhlOfCxY1a7mkJSBta2vunwdB04t89/1O/w1cDnyilFU=';
+  $access_token = '';
   $url = 'https://api.line.me/v1/oauth/verify';
   $headers = array('Authorization: Bearer ' . $access_token);
   $ch = curl_init($url);curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
